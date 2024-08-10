0141c11dfd20d983de5b6516c67610edd766ea67
Ansh1vohra <anshvohra1@gmail.com>
2024-08-10 11:54:10 +0530

    creatingUser

--- api/index.py
+++ api/index.py
@@ -1,10 +1,51 @@
 from fastapi import FastAPI
-# from mangum import Mangum
+from pydantic import BaseModel
+import joblib 
+from motor.motor_asyncio import AsyncIOMotorClient
 
 app = FastAPI()
 
-@app.get("/")
-def read_root():
-    return {"message": "Hello, World!"}
+MONGODB_URI="mongodb+srv://anshvohra1:mongoDB.password01@cluster0.m6bujl9.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0"
 
-# handler = Mangum(app)
+
+client = AsyncIOMotorClient(MONGODB_URI)
+db = client.codeathon
+users_collection = db.Users
+
+# model = joblib.load("model.pkl")
+
+class RequestData(BaseModel):
+    time: str
+    machine: str
+    component: str
+    parameter: str
+    value: float
+
+@app.get("/")
+def Home():
+    return {"message": "Server Team 92 208C Code-a-Thon."}
+
+# @app.post("/process")
+# def process_data(data: RequestData):
+#     input_data = [[data.time, data.machine, data.component, data.parameter, data.value]]
+    
+#     prediction = model.predict(input_data)
+#     return {
+#         # "time": data.time,
+#         # "machine": data.machine,
+#         # "component": data.component,
+#         # "parameter": data.parameter,
+#         # "value": data.value,
+#         "prediction": prediction.tolist() 
+#     }
+
+class User(BaseModel):
+    name: str
+    email: str
+
+@app.post("/users")
+async def create_user(user: User):
+    user_dict = user.model_dump()
+    result = await users_collection.insert_one(user_dict)
+
+    return {"id": str(result.inserted_id), "message": "User created successfully"}
